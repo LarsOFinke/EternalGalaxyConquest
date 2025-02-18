20bd556ecd7ab8459bd2883cd9b7e26a86389aa9
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-02-18 11:58:59 +0100

    Added general.js file to separate scripts

---
+++ frontend/static/script/general.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+function createErrorBox(message) {
+    const error_box = document.createElement("div");
+    error_box.id = "error_box";
+    const error_message = document.createElement("p");
+    error_message.textContent = message;
+    error_box.insertAdjacentElement("afterbegin", error_message);
+    const error_anchor = document.querySelector("form h1");
+    error_anchor.insertAdjacentElement("afterend", error_box);
+};
--- frontend/static/script/index.js
+++ frontend/static/script/index.js
@@ -66,17 +66,6 @@ document.querySelector(".wrapper form").addEventListener("submit", e => {
 });
 
 
-function createErrorBox(message) {
-    const error_box = document.createElement("div");
-    error_box.id = "error_box";
-    const error_message = document.createElement("p");
-    error_message.textContent = message;
-    error_box.insertAdjacentElement("afterbegin", error_message);
-    const error_anchor = document.querySelector("form h1");
-    error_anchor.insertAdjacentElement("afterend", error_box);
-};
-
-
 function toggleShowPasswords() {
     const passwordInput = document.getElementById('password');
     const passwordConfirm = document.getElementById('password2')
