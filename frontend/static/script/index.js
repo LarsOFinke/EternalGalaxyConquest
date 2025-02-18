ddacdb7eca4cc01713b7afbc8780de8b9dedef68
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-02-18 09:49:11 +0100

    Small refactoring in frontend area. Added stylesheets-block in jinja

--- frontend/static/script/index.js
+++ frontend/static/script/index.js
@@ -68,13 +68,11 @@ function toggleShowPasswords() {
 
 function toggleForms() {
     const header = document.querySelector(".wrapper form h1");
-    const show_pw_label = document.querySelector("#show-password label");
     const button = document.querySelector(".wrapper form button");
     const anchor = document.querySelector(".register-link p a");
 
     if (header.textContent === "Login") {
         header.textContent = "Registrierung";
-        show_pw_label.textContent = "Passwörter anzeigen";
         button.textContent = "Registrieren";
         button.id = "register";
         anchor.textContent = "Bereits registriert?";
@@ -92,7 +90,6 @@ function toggleForms() {
         document.getElementById("show-password").insertAdjacentElement("beforebegin", pw2_box);
     } else {
         header.textContent = "Login";
-        show_pw_label.textContent = "Passwort anzeigen";
         button.textContent = "Einloggen";
         button.id = "login";
         anchor.textContent = "Noch nicht registriert?";
