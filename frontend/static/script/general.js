54fdeab76ed00ffb3c2967801c6e722a55ad19b5
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-02-19 21:35:53 +0100

    Small frontend refactoring - added deletion of error_box upon new error_box, if one already exists

--- frontend/static/script/general.js
+++ frontend/static/script/general.js
@@ -2,6 +2,11 @@
 
 
 function createErrorBox(message) {
+    const errorbox = document.getElementById("error_box");
+    if (errorbox !== null) {
+        errorbox.remove();
+    }
+    
     const error_box = document.createElement("div");
     error_box.id = "error_box";
     const error_message = document.createElement("p");
