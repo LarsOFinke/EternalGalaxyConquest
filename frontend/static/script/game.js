e2b90c0baf5e09a216da630dff35d259e549b8e5
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-01 16:20:35 +0100

    Game field is spawning via js-for-loop now with tile-id's.

--- frontend/static/script/game.js
+++ frontend/static/script/game.js
@@ -2,88 +2,23 @@
 
 
 
-const game_field = `
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color2"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color3"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-<div class="hex color1"></div>
-`
-
 function spawn_game_field() {
-    document.getElementById("game_field").innerHTML = game_field;
+    const game_field = document.getElementById("game_field");
+
+    for (let i=1; i<=77; i++) {
+        let new_tile = document.createElement("div");
+        new_tile.id = `tile_${i}`;
+
+        if (i === 34) {
+            new_tile.classList = "hex color2";
+        } else if (i === 44) {
+            new_tile.classList = "hex color3";
+        } else {
+            new_tile.classList = "hex color1";
+        }
+
+        game_field.insertAdjacentElement("beforeend", new_tile);
+    }
 };
 
 spawn_game_field()
