bc827685fd84c0311fc74e4b4387495809c5ecc6
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-01 18:24:11 +0100

    Slowly building core model-classes.

--- frontend/static/script/models/Game.js
+++ frontend/static/script/models/Game.js
@@ -3,13 +3,49 @@
 
 
 class Game {
-    constructor(players) {
+    constructor(players = ["AI", "Player 1"], tile_list = []) {
         this.players = players;
+        this.tile_list = tile_list;
+        if (this.tile_list.length === 0) {
+            this.insertDefaultTiles();
+        }
         this.player_count = players.length;
         this.current_player = 1;
         this.game_state = {};
         this.running = false;
     }
 
+
+    insertDefaultTiles() {
+        for (let i=1; i<=77; i++) {
+            if (i === 34) {
+                this.tile_list.push({
+                    "tile": i,
+                    "tile_type": "home_planet",
+                    "owner": this.players[0],
+                    "tile_content": {
+                        "planet": "'AI' Home Planet"
+                    }
+                });
+            } else if (i === 44) {
+                this.tile_list.push({
+                    "tile": i,
+                    "tile_type": "home_planet",
+                    "owner": this.players[1],
+                    "tile_content": {
+                        "planet": "'Player 1' Home Planet"
+                    }
+                });
+            } else {
+                this.tile_list.push({
+                    "tile": i,
+                    "tile_type": "space",
+                    "owner": "free",
+                    "tile_content": {}
+                });
+            }
+        }
+        
+    }
 
 }
