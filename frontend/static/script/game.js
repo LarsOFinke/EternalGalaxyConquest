f3656c5e4ce951c501257c602a0c1afe621aa00b
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-02 17:20:40 +0100

    Added frontend to build builders hut in the build menu.

--- frontend/static/script/game.js
+++ frontend/static/script/game.js
@@ -145,7 +145,24 @@ function spawnBuildMenu() {
     close_btn.textContent = "X";
     close_btn.addEventListener("click", e => document.getElementById("build-menu-container").remove());
     build_menu_container.insertAdjacentElement("afterbegin", close_btn);
+    
+    // HOME-PLANET MENU //
+    const build_builders_hut_input_box = document.createElement("div");
+    build_builders_hut_input_box.className = "small-input-box";
+    const build_builders_hut_lbl = document.createElement("label");
+    build_builders_hut_lbl.id = "build-builders-hut-lbl";
+    build_builders_hut_lbl.textContent = "Bauhütte";
+    build_builders_hut_input_box.insertAdjacentElement("afterbegin", build_builders_hut_lbl);
+    const build_builders_hut_btn = document.createElement("button");
+    build_builders_hut_btn.id = "build-builders-hut-btn";
+    build_builders_hut_btn.className = "btn-small";
+    build_builders_hut_btn.textContent = "Bauen";
+    build_builders_hut_input_box.insertAdjacentElement("beforeend", build_builders_hut_btn);
+    build_menu_container.insertAdjacentElement("beforeend", build_builders_hut_input_box);
+
     document.getElementById("next-round").insertAdjacentElement("afterend", build_menu_container);
+
+
 };
 
 
