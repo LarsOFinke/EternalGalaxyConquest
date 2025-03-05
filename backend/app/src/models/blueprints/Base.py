bae44134c29771474e381829a82940a6d60c7b80
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-05 18:13:53 +0100

    Fixed some small code-layout issues

--- backend/app/src/models/blueprints/Base.py
+++ backend/app/src/models/blueprints/Base.py
@@ -1,9 +1,18 @@
+from ..locations.Outpost import Outpost
+from ..locations.City import City
+
+
 
 class Base():
     base_count: int = 0
     
     def __init__(self, name: str, base_type: str, tile_id: int, settlements: list):
         Base.base_count += 1
+        self.action_list: list[dict] =  [
+                                            { "name": "Select Settlement", "action": self.select_settlement },
+                                            { "name": "Found City", "action": self.found_city },
+                                            { "name": "Found Outpost", "action": self.found_outpost }
+                                        ]
         self.__base_id: int = Base.base_count
         self.base_type = base_type
         self.name: str = name
@@ -28,4 +37,47 @@ class Base():
             "base_type": self.base_type,
             "settlement_states": [settlement.fetch_settlement_state() for settlement in self.__settlements]
         }
+  
+    
+    
+    def match_payload_action(self, action: str, context: list) -> dict:
+        try:
+            for act in self.action_list:
+                if act["name"] == action:
+                    return act["action"](*context)
+        except Exception as e:
+            return  { "success": False, "message": f"{e}" }
+    
+    
+    def select_settlement(self, target) -> dict:
+        for settlement in self.__settlements:
+            if settlement.name == target:
+                return  { "success": True, "target": settlement }
+        
+        return  { "success": False, "message": f"{target} nicht gefunden!" }
+    
+    
+    def found_outpost(self) -> dict:
+        try:
+            new_outpost = Outpost()
+            self.__settlements.append(new_outpost)
+            return  { "success": True, "message": f"Neuer Außenposten erfolgreich gegründet auf: {self.name}!" }
+        
+        except Exception as e:
+            return  { "success": False, "message": f"Neuer Außenposten konnte nicht gegründet werden auf: {self.name}!\n{e}" }
+
+
+    def found_city(self, name: str, 
+                   gold: float, food: float, wood: float, iron: float, 
+                   buildings: list = [], 
+                   population: list = []
+                   ) -> dict:
+        try:
+            new_city = City(name, gold, food, wood, iron, buildings, population)
+            self.__settlements.append(new_city)
+            return  { "success": True, "message": f"Neue Stadt erfolgreich gegründet auf: {self.name}!" }
+        
+        except Exception as e:
+            return  { "success": False, "message": f"Neue Stadt konnte nicht gegründet werden auf: {self.name}!\n{e}" }
+
 
--- backend/app/src/models/locations/Planet.py
+++ backend/app/src/models/locations/Planet.py
@@ -1,60 +1,8 @@
 from ..blueprints.Base import Base
-from .Outpost import Outpost
-from .City import City
 
 
 
 class Planet(Base):
     def __init__(self, name: str, base_type = "planet", tile_id: int = 0, settlements: list = []):
         super().__init__(name=name, base_type=base_type, tile_id=tile_id, settlements=settlements)
-        self.action_list: list[dict] =  [
-                                            { "name": "Select Settlement", "action": self.select_settlement },
-                                            { "name": "Found City", "action": self.found_city },
-                                            { "name": "Found Outpost", "action": self.found_outpost }
-                                        ]
-        
-    # def fetch_base_state(self) -> dict:
-    #     return {
-    #     }
-      
-    
-    
-    def match_payload_action(self, action: str, context: list) -> dict:
-        try:
-            for act in self.action_list:
-                if act["name"] == action:
-                    return act["action"](*context)
-        except Exception as e:
-            return  { "success": False, "message": f"{e}" }
-    
-    
-    def select_settlement(self, target) -> dict:
-        for settlement in self.__settlements:
-            if settlement.name == target:
-                return  { "success": True, "target": settlement }
-        
-        return  { "success": False, "message": f"{target} nicht gefunden!" }
-    
-    
-    def found_outpost(self) -> dict:
-        try:
-            new_outpost = Outpost()
-            self.__settlements.append(new_outpost)
-            return  { "success": True, "message": f"Neuer Außenposten erfolgreich gegründet auf: {self.name}!" }
-        
-        except Exception as e:
-            return  { "success": False, "message": f"Neuer Außenposten konnte nicht gegründet werden auf: {self.name}!\n{e}" }
 
-
-    def found_city(self, name: str, 
-                   gold: float, food: float, wood: float, iron: float, 
-                   buildings: list = [], 
-                   population: list = []
-                   ) -> dict:
-        try:
-            new_city = City(name, gold, food, wood, iron, buildings, population)
-            self.__settlements.append(new_city)
-            return  { "success": True, "message": f"Neue Stadt erfolgreich gegründet auf: {self.name}!" }
-        
-        except Exception as e:
-            return  { "success": False, "message": f"Neue Stadt konnte nicht gegründet werden auf: {self.name}!\n{e}" }
