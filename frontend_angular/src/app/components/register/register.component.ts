1159523d3b53f7fc525672d7cb044cb14e0a6f09
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-13 22:14:10 +0100

    Added register-page html and css

--- frontend_angular/src/app/components/register/register.component.ts
+++ frontend_angular/src/app/components/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  password: string = '';
+  password_confirm: string = '';
+  showPassword: boolean = false;
 
+  toggleShowPasswords(event: any): void {
+    // Toggle the showPassword value based on checkbox state
+    this.showPassword = event.target.checked;
+  };
 }
