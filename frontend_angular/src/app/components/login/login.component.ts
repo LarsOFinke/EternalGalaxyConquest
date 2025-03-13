1ddd5423877c343477f489c09fc7d15906820b21
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-13 21:23:17 +0100

    Added register-component, to be filled with content but already connected to the login-form and routing

--- frontend_angular/src/app/components/login/login.component.ts
+++ frontend_angular/src/app/components/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    FormsModule
+    FormsModule,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   password: string = '';
-  passwordConfirm: string = '';
   showPassword: boolean = false;
 
   toggleShowPasswords(event: any): void {
@@ -19,77 +20,9 @@ export class LoginComponent {
     this.showPassword = event.target.checked;
   };
 
-  toggleForms() {
-    
-  };
 }
 
 
-
-// const header = document.querySelector(".wrapper form h1");
-    // const button = document.querySelector(".wrapper form button");
-    // const anchor = document.querySelector(".register-link p a");
-
-    // if (header.textContent === "Login") {
-    //     header.textContent = "Registrierung";
-    //     button.textContent = "Registrieren";
-    //     button.id = "register";
-    //     anchor.textContent = "Bereits registriert?";
-
-    //     const pw2_box = document.createElement("div");
-    //     pw2_box.setAttribute("class", "input-box");
-    //     pw2_box.id = "pw2_box";
-        
-    //     const pw2_input = document.createElement("input");
-    //     pw2_input.id = "password2";
-    //     pw2_input.type = "password";
-    //     pw2_input.placeholder = "Passwort bestätigen";
-    //     pw2_input.required = true;
-    //     pw2_box.insertAdjacentElement("afterbegin", pw2_input);
-    //     document.getElementById("show-password").insertAdjacentElement("beforebegin", pw2_box);
-
-    //     const spacer_list = document.querySelectorAll(".spacer")
-    //     spacer_list.forEach(e => e.remove());
-    // } else {
-    //     header.textContent = "Login";
-
-    //     const spacer_1 = document.createElement("br")
-    //     const spacer_2 = document.createElement("br")
-    //     const spacer_3 = document.createElement("br")
-    //     const spacer_4 = document.createElement("br")
-    //     spacer_1.classList.add("spacer");
-    //     spacer_2.classList.add("spacer");
-    //     spacer_3.classList.add("spacer");
-    //     spacer_4.classList.add("spacer");
-    //     document.querySelector("h1").insertAdjacentElement("beforebegin", spacer_1);
-    //     document.querySelector("h1").insertAdjacentElement("afterend", spacer_2);
-    //     document.getElementById("show-password").insertAdjacentElement("beforebegin", spacer_3);
-    //     document.getElementById("show-password").insertAdjacentElement("afterend", spacer_4);
-
-    //     button.textContent = "Einloggen";
-    //     button.id = "login";
-    //     anchor.textContent = "Noch nicht registriert?";
-    //     document.getElementById("pw2_box").remove();
-    // }
-
-    // document.getElementById("username").value = "";
-    // document.getElementById("password").value = "";
-    // const errorbox = document.getElementById("error_box");
-    // if (errorbox !== null) {
-    //     errorbox.remove();
-    // }
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
 // document.querySelector(".wrapper form").addEventListener("submit", e => {
@@ -157,72 +90,3 @@ export class LoginComponent {
 //     }
     
 // });
-
-
-// function toggleShowPasswords() {
-//   const passwordInput = document.getElementById('password');
-//   const passwordConfirm = document.getElementById('password2')
-//   const showPasswordCheckbox = document.getElementById('showPassword');
-
-//   passwordInput.type = showPasswordCheckbox.checked === true ? "text" : "password";
-//   if (passwordConfirm !== null) {
-//       passwordConfirm.type = showPasswordCheckbox.checked === true ? "text" : "password";
-//   }
-// };
-
-
-
-// function toggleForms() {
-//     const header = document.querySelector(".wrapper form h1");
-//     const button = document.querySelector(".wrapper form button");
-//     const anchor = document.querySelector(".register-link p a");
-
-//     if (header.textContent === "Login") {
-//         header.textContent = "Registrierung";
-//         button.textContent = "Registrieren";
-//         button.id = "register";
-//         anchor.textContent = "Bereits registriert?";
-
-//         const pw2_box = document.createElement("div");
-//         pw2_box.setAttribute("class", "input-box");
-//         pw2_box.id = "pw2_box";
-        
-//         const pw2_input = document.createElement("input");
-//         pw2_input.id = "password2";
-//         pw2_input.type = "password";
-//         pw2_input.placeholder = "Passwort bestätigen";
-//         pw2_input.required = true;
-//         pw2_box.insertAdjacentElement("afterbegin", pw2_input);
-//         document.getElementById("show-password").insertAdjacentElement("beforebegin", pw2_box);
-
-//         const spacer_list = document.querySelectorAll(".spacer")
-//         spacer_list.forEach(e => e.remove());
-//     } else {
-//         header.textContent = "Login";
-
-//         const spacer_1 = document.createElement("br")
-//         const spacer_2 = document.createElement("br")
-//         const spacer_3 = document.createElement("br")
-//         const spacer_4 = document.createElement("br")
-//         spacer_1.classList.add("spacer");
-//         spacer_2.classList.add("spacer");
-//         spacer_3.classList.add("spacer");
-//         spacer_4.classList.add("spacer");
-//         document.querySelector("h1").insertAdjacentElement("beforebegin", spacer_1);
-//         document.querySelector("h1").insertAdjacentElement("afterend", spacer_2);
-//         document.getElementById("show-password").insertAdjacentElement("beforebegin", spacer_3);
-//         document.getElementById("show-password").insertAdjacentElement("afterend", spacer_4);
-
-//         button.textContent = "Einloggen";
-//         button.id = "login";
-//         anchor.textContent = "Noch nicht registriert?";
-//         document.getElementById("pw2_box").remove();
-//     }
-
-//     document.getElementById("username").value = "";
-//     document.getElementById("password").value = "";
-//     const errorbox = document.getElementById("error_box");
-//     if (errorbox !== null) {
-//         errorbox.remove();
-//     }
-// };
