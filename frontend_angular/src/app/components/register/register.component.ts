35a2679489afdae3a57774bc879f19ca9acb578e
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-14 01:49:40 +0100

    Registration-page is now fully functional

--- frontend_angular/src/app/components/register/register.component.ts
+++ frontend_angular/src/app/components/register/register.component.ts
@@ -1,23 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { ErrorBoxComponent } from '../general_components/error-box/error-box.component';
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-register',
   imports: [
     RouterLink,
-    FormsModule
+    FormsModule,
+    ErrorBoxComponent
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  hasError: boolean = false;
+  message: string = '';
+  username: string = '';
   password: string = '';
   password_confirm: string = '';
   showPassword: boolean = false;
+
+  private registerService = inject(RegisterService);
 
   toggleShowPasswords(event: any): void {
     // Toggle the showPassword value based on checkbox state
     this.showPassword = event.target.checked;
   };
+
+  register() {
+    this.registerService.register(this.username, this.password).subscribe({
+      next: (response) => {
+        console.log('Register-API-Call successfull', response);
+        // if (response.success) {
+          console.log('Registration successful');
+        // }
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+      }
+    });
+  }
 }
