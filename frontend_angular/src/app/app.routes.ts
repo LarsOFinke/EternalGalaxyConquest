6b92c04cd295d3ca29e7c07d7750f55d20928086
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-03-14 02:40:10 +0100

    Added admin-panel and fixed the a-element-color

--- frontend_angular/src/app/app.routes.ts
+++ frontend_angular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { GameComponent } from './components/game/game.component';
+import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
 
 export const routes: Routes = [
     { path: "", component: LoginComponent },
     { path: "register", component: RegisterComponent },
     { path: "main-menu", component: MainMenuComponent },
-    { path: "game", component: GameComponent }
+    { path: "game", component: GameComponent },
+    { path: "admin-panel", component: AdminPanelComponent }
 ];
